339bc2a7bba0888cf5f48a9dfa6af6fd6998935c
Ali Saydam <alsydm091@gmail.com>
2022-08-17 17:16:32 +0300

    language flag added

--- components/Locale-switcher.js
+++ components/Locale-switcher.js
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useTranslation } from "react-i18next";
@@ -13,9 +14,15 @@ export default function LocaleSwitcher() {
   return (
     <div tabIndex="0" className={styles.languages + " mr-4"}>
       <div className="flex gap-2">
-        <p className="border-solid border-2 border-btnGreen px-2  md:w-32">
-          {activeLocale.toLocaleUpperCase()} ▼
-        </p>
+        <div className="border-solid border-2 border-btnGreen px-1 gap-2 flex ">
+          <Image
+            src={`/${activeLocale}.png`}
+            width="25"
+            height={25}
+            alt="flag"
+          />
+          <p className="">{activeLocale.toLocaleUpperCase()}▼</p>
+        </div>
         <p
           tabIndex="1"
           className="border-solid border-2 text-txtGreen font-semibold border-btnGreen bg-btnGreen px-2"
@@ -30,8 +37,16 @@ export default function LocaleSwitcher() {
             <li key={locale}>
               <Link href={{ pathname, query }} as={asPath} locale={locale}>
                 <div className={styles.lang_con}>
-                  <a className="p-2 hover:bg-sky-700 px-6 md:px-10">
-                    {locale.toLocaleUpperCase()}
+                  <a className="p-2 hover:bg-sky-700 ">
+                    <div className="flex">
+                      <Image
+                        src={`/${locale}.png`}
+                        width="25"
+                        height={25}
+                        alt="flag"
+                      />
+                      <p className="">{locale.toLocaleUpperCase()} ▼</p>
+                    </div>
                   </a>
                 </div>
               </Link>
